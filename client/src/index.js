6bafc3aeb47fee04e801821a7d7e221f4cfdc467
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2022-04-09 23:29:48 +0200

    payments with ether

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,73 @@
 const updateBalance = async (contract, accounts) => {
     balance = await contract.methods.balanceOf(accounts[0]).call();
-    $("p").html(`Balance of account <b>${accounts[0]}</b> is <b>${balance / 10 ** 18} MTK</b>`);
+    $("#balance").html(`Balance of account <b>${accounts[0]}</b> is <b>${balance / 10 ** 18} MTK</b>`);
+	$("#tokens").attr("max", balance / 10 ** 18);
 };
 
 const buySell = (contract, accounts) => {
     $("#buy").on("click", async (e) => {
+		e.preventDefault();
         console.log("buy clicked");
-        // input = e.target.value;
-        e.preventDefault();
-        await contract.methods
-            .buy(BigInt(1 * 10 ** 18))
-            .send({ from: accounts[0]});
-        updateBalance(contract, accounts);
+
+        try {
+            const transactionHash = await ethereum.request({
+              method: 'eth_sendTransaction',
+              params: [
+                {
+                  to: '0x4222EFD2a4bC0F60D6eABEd33a7bEB3cc8c1a096',
+                  from: accounts[0],
+                  value: (10000000000000000n).toString(16),
+                },
+              ],
+            });
+            // Handle the result
+            console.log(transactionHash);
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({hash: transactionHash}),
+            }
+            
+            fetch('http://localhost:8000/api', options)
+				.then((res) => {
+            		updateBalance(contract, accounts);
+					console.log(res.statusText)
+            	});
+        } catch (error) {
+        	console.error(error);
+        }
     });
     $("#sell").on("click", async (e) => {
         console.log("sell clicked");
         e.preventDefault();
+		tokens = $("#tokens").val();
+
+		// const tx = contract.methods.burn(BigInt(tokens * 10 ** 18));
+		// const data = tx.encodeABI();
+
+		// try {
+        //     const transactionHash = await ethereum.request({
+        //       method: 'eth_sendTransaction',
+        //       params: [
+        //         {
+        //           to: '0x35fC5E48339C7fE7ebA306DE11dE50824B4C7F9f',
+        //           from: accounts[0],
+        //           value: (BigInt(10000000000000000 - (tokens * 0.1))).toString(16),
+		// 		  data: data
+        //         },
+        //       ],
+        //     });
+        //     // Handle the result
+        //     console.log(transactionHash);
+        // } catch (error) {
+        // 	console.error(error);
+        // }
+
         await contract.methods
-            .burn(BigInt(1 * 10 ** 18))
-            .send({ from: accounts[0]});
+            .burn(BigInt(tokens * 10 ** 18))
+            .send({ from: accounts[0] });
         updateBalance(contract, accounts);
     });
 };
